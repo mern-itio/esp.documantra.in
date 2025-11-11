c69b07da53d5e913b5897a68d1ad7f7b78587bd0
pawnesh <pawneshkitio@gmail.com>
2025-11-11 16:45:55 +0530

    Signing Porcess fixed

--- Backend/services/e-sign-service/routes/publicRoutes.js
+++ Backend/services/e-sign-service/routes/publicRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 // Configure multer storage (files go to /uploads folder)
-const { envelopesDetail,getSignatureFields,addSignature, activityLogs,getEnvelopePower,signerInitiate,getSelfSigner } = require('../controllers/mainController');
+const { envelopesDetail,getSignatureFields,addSignature, activityLogs,getEnvelopePower,signerInitiate,getSelfSigner,saveNonSignatureField } = require('../controllers/mainController');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, '..', 'uploads')); // one level up from /routes
@@ -23,6 +23,7 @@ router.get('/envelope/activity-log/:envelopeId', activityLogs);
 router.get('/envelope/power/:powerFormId/:envelopeId',getEnvelopePower);
 router.post('/envelope/signer-initiate',signerInitiate);
 router.get('/envelope/self-signer/:cycleId',getSelfSigner);
+router.post('/save-non-signature-field', saveNonSignatureField);
 
 
 module.exports = router;
