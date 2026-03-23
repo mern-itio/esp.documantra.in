6eeac27b430a66926f764506d6bf8d8b8fc81add
Sneha-tiwari-itio <snehat@itio.in>
2026-03-23 18:23:07 +0530

    Add ShieldCheck icon import to SignerStatusPage

--- Frontend/src/pages/eSign/SignerStatusPage.tsx
+++ Frontend/src/pages/eSign/SignerStatusPage.tsx
@@ -9,6 +9,7 @@ import {
   Check,
   Link as LinkIcon,
   Users,
+  ShieldCheck, //added
 } from "lucide-react";
 import { authApi, eSignApi } from "../../services/apiHelper";
 import confetti from "canvas-confetti";
